a97c15fba51d9e525fa343ddce4be63eeaa94930
Zyaqk <ukaminskiy06@gmail.com>
2023-11-29 17:32:59 +0200

    Updates 1.1

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
+const folder = 'data';
+const dataPath = path.join(__dirname, folder);
+
+const server = http.createServer((request, response) => {
+    if(request.url == '/jokes' && request.method == 'GET') {
+        getAllJokes(request, response);
+    }
+});
+server.listen(3000);
+
+
+function getAllJokes(request, response) {
+    let dir = fs.readdirSync(dataPath);
+    let allJokes = [];
+
+    for(let i = 0; i < dir.length; i++) {
+        let file = fs.readFileSync(path.join(dataPath, i+ '.json'));
+        let jokeJson = Buffer.from(file).toString();
+        let joke = JSON.parse(jokeJsoN);
+        joke.id = i;
+
+        allJokes.push(joke);
+    }
+    response.end(JSON.stringify(allJokes));
+}
